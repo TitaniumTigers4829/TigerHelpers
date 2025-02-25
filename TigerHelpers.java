47b22e8cbede12a498e4ed6c1192fbd33ce0d123
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-02-25 12:51:04 -0500

    bruh

--- TigerHelpers.java
+++ TigerHelpers.java
@@ -30,43 +30,22 @@ public class TigerHelpers {
 
   private static final Map<String, DoubleArrayEntry> doubleArrayEntries = new ConcurrentHashMap<>();
 
-  /** Represents a Limelight Raw Fiducial result from Limelight's NetworkTables output. */
-  public record RawFiducial {
-    /** The id of the april tag */
-    public int id = 0;
-
-    /**
-     * The horizontal offset of the target from the Limelight's principle pixel (the images central
-     * pixel) in degrees. Positive values mean the target is to the right of the crosshair.
-     */
-    public double txnc = 0;
-
-    /**
-     * The vertical offset of the target from the Limelight's principle pixel (the images central
-     * pixel) in degrees. Positive values mean the target is below the crosshair.
-     */
-    public double tync = 0;
-
-    /**
-     * The area of the target in the Limelight's view as a percentage of the total image area. So,
-     * if the target takes up 10% of the image, this value will be 0.1.
-     */
-    public double ta = 0;
-
-    /** The distance from the Limelight to the april tag in meters. */
-    public double distToCamera = 0;
-
-    /** The distance from the robot to the april tag in meters. */
-    public double distToRobot = 0;
-
-    /**
-     * The ambiguity of the april tag. This ranges from 0 to 1, a lower values meaning less
+  /** Represents a Limelight Raw Fiducial result from Limelight's NetworkTables output. 
+   * 
+   * @param id The id of the april tag
+   * @param txnc The horizontal offset of the target from the Limelight's principle pixel (the images central pixel) in degrees.
+   * @param tync The vertical offset of the target from the Limelight's principle pixel (the images central pixel) in degrees.
+   * @param ta The area of the target in the Limelight's view as a percentage of the total image area.
+   * @param distToCamera The distance from the Limelight to the april tag in meters.
+   * @param distToRobot The distance from the robot to the april tag in meters.
+   * @param ambiguity  The ambiguity of the april tag. This ranges from 0 to 1, a lower values meaning less
      * ambiguous. The higher this is, the more likely it is that you will see ambiguity flipping.
      * Ambiguity flipping is when there are multiple possible "solutions" for the pose estimate, so
      * if this is higher your estimate will be less reliable. The best way to reduce this it to make
      * the april tag look as trapezoidal as possible from the Limelight's perspective.
-     */
-    public double ambiguity = 0;
+  */
+  public record RawFiducial(int id, double txnc, double tync, double ta, double distToCamera, double distToRobot, double ambiguity) {
+
 
     /**
      * Initializes a RawFidicual with the provided values. This represents data from reading a
